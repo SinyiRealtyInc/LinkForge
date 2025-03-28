a4d6c000eff26280f43e984bf8ffe5f2f1bba9bf
Ray <s384861@sinyi.com.tw>
2025-03-28 18:10:33 +0800

    -add: omnichat router lineFlexMessageWithParameter

--- routes/omnichat_routes.js
+++ routes/omnichat_routes.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/lineFlexMessage", (req, res) => {
-  res.status(200).send("Good");
+router.get("/lineFlexMessageWithParameter", (req, res) => {
+  res.status(200).json({
+    "messages": [
+      {
+        "type": "lineFlexMessageTemplates",
+        "lineFlexMessageTemplates": [
+          {
+            "templateId": "67e37ac3bcb81f09e0394cd7",
+            "parameters": [
+              {
+                "key": "button_title",
+                "value": "測試變數"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  });
 });
 
 module.exports = router; 
